6e3ff486c5f60d6d4a16a58022ee9107df3b89ad
grit0614 <grit0614@gmail.com>
2016-04-10 00:10:38 +0000

    Rev. 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,12 +11,12 @@ KONKUK_BEGIN = 54
 KONKUK_END = 96
 
 # Designates the starting/ending point of the parsing process
-GO = KONKUK_BEGIN
-STOP = KONKUK_END
+GO = 1
+STOP = 1227
 
 class HomeController < ApplicationController
 
-    def index
+    def parse
         
         # Classlion Login module using Mechanize
         a = Mechanize.new
@@ -30,24 +30,47 @@ class HomeController < ApplicationController
         @arr_univ = Array.new
         @arr_pic = Array.new
         
+        
+        ################################################################
+        #                                                              #
+        #              Beginning of Parsing Componenent                #
+        #                                                              #
+        ################################################################
+        
         # Parse the desired data
         for i in GO..STOP
             a.get(PAGE_URL + i.to_s)
             @arr_name[i] = a.page.parser.css('div.user-profile a')[0].text
-            @arr_univ[i] = a.page.parser.css('div.user-profile span.univ')[0].text.delete "|"
+            @arr_univ[i] = a.page.parser.css('div.user-profile span.univ')[0].text.delete "| "
             
             if a.page.parser.css('div.user-photo img')[0].attr('src').chr != "/"
                 @arr_pic[i] = a.page.parser.css('div.user-photo img')[0].attr('src')
             end
             
             # output test
-            puts @arr_name[i] + "   " + @arr_univ[i] + "     " + @arr_pic[i].to_s
+            # puts @arr_name[i] + "   " + @arr_univ[i] + "     " + @arr_pic[i].to_s
+        
+            c = Contact.new
+            c.id = i
+            c.name = @arr_name[i].to_s
+            c.univ = @arr_univ[i].to_s
+            c.pic = @arr_pic[i].to_s
+            c.save
+            
         end
+        
+        ################################################################
+        #                                                              #
+        #                 End of Parsing Componenent                   #
+        #                                                              #
+        ################################################################
         
         # batch operation test
         # @arr_name.map {|x| to_s}
         # @arr_univ.map {|x| to_s}
         # @arr_pic.map {|x| to_s}
-    
+        
+        # puts Contact.all
+        
     end
 end
